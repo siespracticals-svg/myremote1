ce27d0f83d4586f5c53c2903ae75098e392fbf2c
siespracticals-svg <siespracticals@gmail.com>
2026-03-11 20:21:35 +0530

    Fix log statement for Jenkins startup message

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 console.log("Node.js project running in Jenkins pipeline");
-console.log("Added statement so that Jenkins will start it");
+console.log("Added statement so that Jenkins will start it ");
